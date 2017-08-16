5ad8bb592f50d21b6856c54cba36262192964bee
jgreenQA <jack.green@qa.com>
2017-08-16 17:01:42 +0000

    Updated Reverse Factorial

--- src/main/scala/factorial/Factorial.scala
+++ src/main/scala/factorial/Factorial.scala
@@ -2,12 +2,12 @@ package factorial
 
 object Factorial {
   def main(args: Array[String]): Unit = {
-    println(getFactorial(120))
+    println(getFactorial(18))
   }
 
   def getFactorial(a: Int): String = {
     var (e, i) = (a, 1)
     while (e != 1 && i < a) {i += 1; e /= i}
-    if (e == 1) s"$a = $i!" else "NONE"
+    if (e == 1) s"$a = $i!" else s"$a = NONE"
   }
 }
